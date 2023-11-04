6f7d2ff020eabc891939ef3afda9305d5b85ee9f
SofiiSerial <sofiaantonellaserial@gmail.com>
2023-11-03 21:58:02 -0300

    creamos un post

--- BACKEND/routes/api/puntajes/main.js
+++ BACKEND/routes/api/puntajes/main.js
@@ -18,12 +18,42 @@ const getToken = function(){
 
 SELECT id_color , SUM(puntaje) FROM `puntaje` WHERE id_color = 3; 
 */
+
+const guardarPuntaje = function (id_juego, color_id, puntaje){
+    return new Promise((resolve, reject) => {
+        // sql insert 
+       const sql = "INSERT INTO puntaje (id_juegos, id_color, puntaje) VALUES (?, ?, ?)"
+       con.query(sql, [id_juego, color_id, puntaje], function(error, result){
+            if(error){
+                res.json({
+                    status:"error",
+                    error
+                })
+            }else{
+
+                res.json({
+                    status:"puntaje",
+                    puntaje: result
+                })
+            }
+        })
+    })
+}
+
+
+router.post("/",function(req, res, next){
+    const {id_juego, color1_id, color1_puntaje, color2_id, color2_puntaje, color3_id, color3_puntaje} = req.body
+    //llamar guardar puntaje para cada color
+
+
+})
+
 router.get("/buscar",function(req, res, next){
-    const {dni} = req.query
-    const sql=`SELECT C.color, SUM(puntaje.puntaje) as "puntaje" FROM "puntaje"
-    INNER JOIN colores AS C ON puntaje.id_color = C.id_color GROUP BY puntaje.id_color;
+    const {} = req.query
+    const sql=`SELECT C.color, SUM(P.puntaje) as puntaje1" FROM puntaje
+    INNER JOIN colores AS C ON P.id_color = C.id_color GROUP BY P.id_color;
     WHERE P.id_color = ?`
-    con.query(sql, [dni], function(error, result){
+    con.query(sql, [], function(error, result){
         if(error){
             res.json({
                 status:"error",
@@ -32,17 +62,17 @@ router.get("/buscar",function(req, res, next){
         }else{
 
             res.json({
-                status:"usuarios",
-                usuarios: result
+                status:"puntaje",
+                puntaje: result
             })
         }
     })
 })
 
 router.get("/",function(req, res, next){
-    //const {id_color} = req.query
-    const sql=`SELECT C.color, SUM(puntaje.puntaje) as "puntaje" FROM "puntaje"
-    INNER JOIN colores AS C ON puntaje.id_color = C.id_color GROUP BY puntaje.id_color;`
+    //const {} = req.query
+    const sql=`SELECT C.color, SUM(P.puntaje) as puntaje FROM puntaje AS P
+    INNER JOIN colores AS C ON P.id_color = C.id_color GROUP BY P.id_color;`
     con.query(sql, [], function(error, result){
         if(error){
             res.json({
